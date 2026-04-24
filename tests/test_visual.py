842d8820488b5a5b893dc10eab8bbc95b0fecf1d
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-04-24 13:44:18 +0100

    add visual test for automate page

--- tests/test_visual.py
+++ tests/test_visual.py
@@ -14,12 +14,28 @@ def get_home_page(mocker):
     response = test_app.get("/")
     return response
 
+def get_automate_page(mocker):
+    mocker.patch("db.get_duties", return_value=mock_duties)
+    response = test_app.get("/automate")
+    return response
+
+
 # visual check for homepage
 def xtest_display_home_page(mocker):
     html_content = get_home_page(mocker).text
     output_dir = Path("test_output")
     output_dir.mkdir(exist_ok=True)
     file_path = output_dir / "home_page_preview.html"
+    file_path.write_text(html_content)
+    webbrowser.open(f"file://{file_path.absolute()}")
+    assert file_path.exists()
+    
+    # visual check for automate page
+def xtest_display_automate_page(mocker):
+    html_content = get_automate_page(mocker).text
+    output_dir = Path("test_output")
+    output_dir.mkdir(exist_ok=True)
+    file_path = output_dir / "automate_page_preview.html"
     file_path.write_text(html_content)
     webbrowser.open(f"file://{file_path.absolute()}")
     assert file_path.exists()
