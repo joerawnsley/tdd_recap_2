705cc078363168afba8e6dc734d810867f252272
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-04-24 12:46:43 +0100

    create automate page route and template

--- app.py
+++ app.py
@@ -9,6 +9,11 @@ def index():
   duties = db.get_duties()
   return render_template("index.html", duties=duties)
 
+@app.route('/automate')
+def automate():
+  duties = db.get_duties()
+  return render_template("automate.html", duties=duties)
+
 
 if __name__ == '__main__':
   app.run(host='0.0.0.0', port=5000)
