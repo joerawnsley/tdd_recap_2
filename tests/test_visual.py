b32e65ae0317b7916d8f02e5c1ef9088b7d7f5a8
Joe Rawnsley <joe.rawnsley@madetech.com>
2026-04-24 12:07:18 +0100

    reorganise tests

---
+++ tests/test_visual.py
@@ -0,0 +1,25 @@
+from app import app
+import webbrowser
+from pathlib import Path
+import json
+
+test_app = app.test_client()
+
+# get home page with mock data
+with open('mock_data/duties.json') as duties:
+    mock_duties = json.load(duties)
+
+def get_home_page(mocker):    
+    mocker.patch("db.get_duties", return_value=mock_duties)
+    response = test_app.get("/")
+    return response
+
+# visual check for homepage
+def test_display_home_page(mocker):
+    html_content = get_home_page(mocker).text
+    output_dir = Path("test_output")
+    output_dir.mkdir(exist_ok=True)
+    file_path = output_dir / "home_page_preview.html"
+    file_path.write_text(html_content)
+    webbrowser.open(f"file://{file_path.absolute()}")
+    assert file_path.exists()
